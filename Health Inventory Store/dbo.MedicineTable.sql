d3539d03121e96ca47cd08776c0ab8f975109421
omercevik <36816752+omercevik@users.noreply.github.com>
2019-07-17 16:28:01 +0300

    Add files via upload

---
+++ Health Inventory Store/dbo.MedicineTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[MedicineTable] (
+    [MedicineId]    INT          NOT NULL,
+    [MedicineName]  VARCHAR (50) NOT NULL,
+    [MedicineUnit]  VARCHAR (20) NOT NULL,
+    [MedicineCount] INT          NOT NULL,
+    [MedicineDate]  DATETIME     NOT NULL,
+    PRIMARY KEY CLUSTERED ([MedicineId] ASC)
+);
+
